7f7fc07a653a18e68dacaa28015dc5af98d7ec0c
Marvis Ighedosa <dosamarvis@gmail.com>
2023-11-30 13:27:33 +0100

    Initial Commit

---
+++ Mesla/MeslaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeslaApp.swift
+//  Mesla
+//
+//  Created by Marvis Ighedosa on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeslaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
